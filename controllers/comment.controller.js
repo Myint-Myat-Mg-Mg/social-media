91c63daa083a125f93f2a725f2d5c4aba6f73510
M4 <myintmyatmgmg373717@gmail.com>
2024-08-30 16:40:37 +0630

    error fix

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 
 export const getComments = async (req, res) => {
     try {
-        const comments = await pirsma.comment.findMany();
+        const comments = await prisma.comment.findMany();
         res.json(comments);
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -14,7 +14,7 @@ export const getComments = async (req, res) => {
 export const getSingleComment = async (req, res) => {
     const {id} = req.params;
     try {
-        const comment = await pirsma.comment.findUnique({
+        const comment = await prisma.comment.findUnique({
             where: { id: Number(id) }
         });
         if (!comment) {
@@ -29,7 +29,7 @@ export const getSingleComment = async (req, res) => {
 export const createComment = async (req, res) => {
     try {
         const { authorId, postId, content  } = req.body;
-        const newComment = await pirsma.comment.create({ 
+        const newComment = await prisma.comment.create({ 
             data: { authorId, postId, content },
         });
         res.json(newComment);
@@ -48,11 +48,11 @@ export const updateComment = async (req, res) => {
         });
 
         if (!comment) {
-            res.status(404).josn({ error: "Comment not found" });
+            res.status(404).json({ error: "Comment not found" });
         }
         res.status(200).json(comment);
     } catch (error) {
-        res.status(500).josn({ error: error.message });
+        res.status(500).json({ error: error.message });
     };
 };
 
