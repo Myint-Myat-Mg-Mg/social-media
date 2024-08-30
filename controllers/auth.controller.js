e061b8ec33371672154fe230fa8f7519e663d577
M4 <myintmyatmgmg373717@gmail.com>
2024-08-30 10:58:59 +0630

    token expire remove

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -14,9 +14,6 @@ export const generateToken = (userName, id) => {
             generateDateTime: new Date()
         },
         process.env.JWT_SECRET,
-        {
-            expiresIn: "1h"
-        }
     );
     return token;
 }
@@ -32,7 +29,7 @@ export const validateToken = (req, res, next) => {
             next(token);
         }
     } catch (err) {
-        res.status(401).json({error: "Invalid or expired token"});
+        res.status(401).json({error: "Invalid token"});
     }
 };
 
@@ -60,7 +57,7 @@ const userLogin = async (req, res) => {
     
             }
     
-            const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, {expiresIn: "1h"});
+            const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET);
         
             res.json({ token });
             } catch (error) {
